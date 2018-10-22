fb8094983ce3076eeccf207f3daae98d1848db72
Satu <satu.salekari@sc5.io>
2018-10-22 11:32:22 +0300

    Add back dropped rows
    These were accidentally dropped during conflict resolution.

--- weather.py
+++ weather.py
@@ -7,7 +7,12 @@ FILTER_WEATHER_HOURS = ['08:00', '09:00', '10:00', '11:00'] #TODO verify this is
 get_daily_mean = lambda df, column: df.groupby(['year', 'month', 'day'])[column].mean().values
 
 def get_daily_weather_data_2016_to_2018():
-
+    weather_data = pd.read_csv('./data/weather_by_hour_2016_to_2018.csv', sep=',')
+    #remove times which are not around lunch time:
+    weather_data = weather_data[weather_data.Time.isin(FILTER_WEATHER_HOURS)]
+    #rename columns:
+    weather_data = weather_data.rename(index=str, columns={"Year": "year", "m": "month", "d": "day"})
+    #initialize empty data frame
     daily_data  = pd.DataFrame(columns = ['date', 'precipitation_intensity', 'cloud_amount', 'air_temperature', 'month'])
     #add date
     daily_data['date'] = pd.to_datetime(weather_data[['year', 'month', 'day']], utc=True).unique()
